202a90740af0145b844f214c6ff2f4dea9038b64
repairvision <46819476+repairvision@users.noreply.github.com>
2019-03-29 14:43:18 +0100

    index pages

--- assets/js/lunr-feed.js
+++ assets/js/lunr-feed.js
@@ -11,7 +11,6 @@ var index = lunr(function () {
 
 {% assign count = 0 %}
 {% for post in site.posts %}
-{% if post.title %}
     index.add({
       title: {{post.title | jsonify}},
       category: {{post.category | jsonify}},
@@ -20,21 +19,10 @@ var index = lunr(function () {
       id: {{count}}
     });
     {% assign count = count | plus: 1 %}
-{% endif %}
 {% endfor %}
 
-var store = [{% for post in site.posts %}{
-{% if post.title %}
-    "title": {{post.title | jsonify}},
-    "link": {{ post.url | jsonify }},
-    "image": {{ post.image | jsonify }},
-    "date": {{ post.date | date: '%B %-d, %Y' | jsonify }},
-    "category": {{ post.category | jsonify }},
-    "excerpt": {{ post.content | strip_html | truncatewords: 20 | jsonify }}
-{% endif %}
-}{% unless forloop.last %},{% endunless %}{% endfor %}]
-
 {% for page in site.pages %}
+{% if post.title %}
     index.add({
       title: {{page.title | jsonify}},
       category: {{page.categories | jsonify}},
@@ -43,15 +31,26 @@ var store = [{% for post in site.posts %}{
       id: {{count}}
     });
     {% assign count = count | plus: 1 %}
+{% endif %}
 {% endfor %}
 
-var store = [{% for page in site.pages %}{
+var store = [{% for post in site.posts %}{
+    "title": {{post.title | jsonify}},
+    "link": {{ post.url | jsonify }},
+    "image": {{ post.image | jsonify }},
+    "date": {{ post.date | date: '%B %-d, %Y' | jsonify }},
+    "category": {{ post.category | jsonify }},
+    "excerpt": {{ post.content | strip_html | truncatewords: 20 | jsonify }}
+{% unless forloop.last %},{% endunless %}{% endfor %}       
+{% for page in site.pages %}
+{% if post.title %}
     "title": {{page.title | jsonify}},
     "link": {{ page.url | jsonify }},
     "image": {{ page.image | jsonify }},
     "date": {{ page.date | date: '%B %-d, %Y' | jsonify }},
     "category": {{ page.categories | jsonify }},
     "excerpt": {{ page.content | strip_html | truncatewords: 20 | jsonify }}
+{% endif %}            
 }{% unless forloop.last %},{% endunless %}{% endfor %}]
 
 $(document).ready(function() {
