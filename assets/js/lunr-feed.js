107d464331a2125d2653a66c4e0c32865ebb1ffe
repairvision <46819476+repairvision@users.noreply.github.com>
2019-03-29 15:10:41 +0100

    page index

--- assets/js/lunr-feed.js
+++ assets/js/lunr-feed.js
@@ -50,7 +50,7 @@ var store = [{% for post in site.posts %}{
     "date": {{ page.date | date: '%B %-d, %Y' | jsonify }},
     "category": {{ page.categories | jsonify }},
     "excerpt": {{ page.content | strip_html | truncatewords: 20 | jsonify }}
-}{% endif %}{% unless forloop.last %},{% endunless %}{% endfor %}]
+}{% unless forloop.last %},{% endunless %}{% endif %}{% endfor %}]
 
 $(document).ready(function() {
     $('#search-input').on('keyup', function () {
