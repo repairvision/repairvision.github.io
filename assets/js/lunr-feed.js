74a399e7e95b53dab4faab70aa2c04886056014a
repairvision <46819476+repairvision@users.noreply.github.com>
2019-03-29 14:36:45 +0100

    filter pages without title

--- assets/js/lunr-feed.js
+++ assets/js/lunr-feed.js
@@ -11,6 +11,7 @@ var index = lunr(function () {
 
 {% assign count = 0 %}
 {% for post in site.posts %}
+{% if post.title %}
     index.add({
       title: {{post.title | jsonify}},
       category: {{post.category | jsonify}},
@@ -19,15 +20,18 @@ var index = lunr(function () {
       id: {{count}}
     });
     {% assign count = count | plus: 1 %}
+{% endif %}
 {% endfor %}
 
 var store = [{% for post in site.posts %}{
+{% if post.title %}
     "title": {{post.title | jsonify}},
     "link": {{ post.url | jsonify }},
     "image": {{ post.image | jsonify }},
     "date": {{ post.date | date: '%B %-d, %Y' | jsonify }},
     "category": {{ post.category | jsonify }},
     "excerpt": {{ post.content | strip_html | truncatewords: 20 | jsonify }}
+{% endif %}
 }{% unless forloop.last %},{% endunless %}{% endfor %}]
 
 {% for page in site.pages %}
