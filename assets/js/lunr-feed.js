7b31145c545950e0c94e16f33a4ccf2a1996760b
repairvision <46819476+repairvision@users.noreply.github.com>
2019-03-29 14:29:46 +0100

    index pages

--- assets/js/lunr-feed.js
+++ assets/js/lunr-feed.js
@@ -21,17 +21,6 @@ var index = lunr(function () {
     {% assign count = count | plus: 1 %}
 {% endfor %}
 
-{% for post in site.pages %}
-    index.add({
-      title: {{post.title | jsonify}},
-      category: {{post.category | jsonify}},
-      content: {{post.content | strip_html | jsonify}},
-      tags: {{post.tags | jsonify}},
-      id: {{count}}
-    });
-    {% assign count = count | plus: 1 %}
-{% endfor %}
-
 var store = [{% for post in site.posts %}{
     "title": {{post.title | jsonify}},
     "link": {{ post.url | jsonify }},
@@ -39,6 +28,26 @@ var store = [{% for post in site.posts %}{
     "date": {{ post.date | date: '%B %-d, %Y' | jsonify }},
     "category": {{ post.category | jsonify }},
     "excerpt": {{ post.content | strip_html | truncatewords: 20 | jsonify }}
+}{% unless forloop.last %},{% endunless %}{% endfor %}]
+
+{% for page in site.pages %}
+    index.add({
+      title: {{page.title | jsonify}},
+      category: {{page.categories | jsonify}},
+      content: {{page.content | strip_html | jsonify}},
+      tags: {{page.tags | jsonify}},
+      id: {{count}}
+    });
+    {% assign count = count | plus: 1 %}
+{% endfor %}
+
+var store = [{% for page in site.pages %}{
+    "title": {{page.title | jsonify}},
+    "link": {{ page.url | jsonify }},
+    "image": {{ page.image | jsonify }},
+    "date": {{ page.date | date: '%B %-d, %Y' | jsonify }},
+    "category": {{ page.categories | jsonify }},
+    "excerpt": {{ page.content | strip_html | truncatewords: 20 | jsonify }}
 }{% unless forloop.last %},{% endunless %}{% endfor %}]
 
 $(document).ready(function() {
